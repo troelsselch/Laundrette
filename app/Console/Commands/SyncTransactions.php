858ed5d9f4abda8123d4ba58fd2e92338e2c55c1
Troels Selch <troels@golthak.dk>
2018-09-15 19:51:48 +0200

    Elaborate on example command. Add env keys to .env.example

--- app/Console/Commands/SyncTransactions.php
+++ app/Console/Commands/SyncTransactions.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Adapters\GuzzleAdapter;
+use App\Http\Controllers\LaundretteController;
 use Illuminate\Console\Command;
 
 class SyncTransactions extends Command
@@ -12,6 +14,23 @@ class SyncTransactions extends Command
 
     public function handle() : void
     {
-        print "Template\n";
+        $url = env('LAUNDRETTE_URL', '');
+        $username = env('LAUNDRETTE_USERNAME', '');
+        $password = env('LAUNDRETTE_PASSWORD', '');
+
+        if (empty($url) || empty($username) || empty($password)) {
+            $this->error('Missing url, username, and/or password configuration.');
+            return;
+        }
+
+        $guzzle = new GuzzleAdapter($url, $username, $password);
+        $laundrette = new LaundretteController($guzzle);
+        $transactions = $laundrette->getTransactions();
+
+        foreach ($transactions as $transaction) {
+            // todo save, if unique
+            print $transaction;
+            print PHP_EOL;
+        }
     }
 }
